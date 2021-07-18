db79bcc3b71be2d9d4ef575617d44ad1b4649eda
Golubev Vladimir <goso@list.ru>
2021-07-18 09:16:01 +0300

    junit4, expected - actual

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,39 +1,39 @@
 package ru.netology.service;
 
-import junit.framework.TestCase;
+//import junit.framework.TestCase;
 
-public class CashbackHackServiceTest extends TestCase {
+public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn100IfUsed900() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(900);
         int expected = 100;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn200IfUsed1800() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1800);
         int expected = 200;
-        org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn0IfUsed0() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(0);
         int expected = 0;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn0IfUsed1000() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
 }
